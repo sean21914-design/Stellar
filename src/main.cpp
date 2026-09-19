cebc2bdd2e7eb124f3fcc3e1627bc400a2a0c2b4
sean21914-design <sean21914@gmail.com>
2026-09-18 23:16:39 -0500

    Create Stellar menu foundation

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,55 @@
 
 using namespace geode::prelude;
 
+class StellarMenu : public FLAlertLayer {
+protected:
+    bool init() {
+        if (!FLAlertLayer::init(
+            0,
+            "Stellar",
+            "Close",
+            nullptr,
+            nullptr
+        )) {
+            return false;
+        }
+
+        auto title = CCLabelBMFont::create("STELLAR", "bigFont.fnt");
+        title->setScale(0.8f);
+        title->setPosition(
+            m_mainLayer->getContentSize().width / 2,
+            m_mainLayer->getContentSize().height - 35
+        );
+        m_mainLayer->addChild(title);
+
+        auto text = CCLabelBMFont::create(
+            "Stellar menu coming soon...",
+            "goldFont.fnt"
+        );
+        text->setScale(0.7f);
+        text->setPosition(
+            m_mainLayer->getContentSize().width / 2,
+            m_mainLayer->getContentSize().height / 2
+        );
+        m_mainLayer->addChild(text);
+
+        return true;
+    }
+
+public:
+    static StellarMenu* create() {
+        auto ret = new StellarMenu();
+
+        if (ret->init()) {
+            ret->autorelease();
+            return ret;
+        }
+
+        delete ret;
+        return nullptr;
+    }
+};
+
 class $modify(StellarMenuLayer, MenuLayer) {
     bool init() {
         if (!MenuLayer::init()) {
@@ -23,16 +72,10 @@ class $modify(StellarMenuLayer, MenuLayer) {
             menu->updateLayout();
         }
 
-        log::info("Stellar loaded successfully!");
-
         return true;
     }
 
     void onStellarButton(CCObject*) {
-        FLAlertLayer::create(
-            "Stellar",
-            "Stellar is alive! ⭐",
-            "OK"
-        )->show();
+        StellarMenu::create()->show();
     }
 };
